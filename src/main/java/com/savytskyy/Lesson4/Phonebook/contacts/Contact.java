127d6a6c957399eaed4c8387b39ce0f41af75eed
Andriy Savytskyy <andy1978dnepr@gmail.com>
2021-01-18 21:20:01 +0200

    added lombok dependency

--- src/main/java/com/savytskyy/Lesson4/Phonebook/contacts/Contact.java
+++ src/main/java/com/savytskyy/Lesson4/Phonebook/contacts/Contact.java
@@ -1,16 +1,21 @@
 package main.java.com.savytskyy.Lesson4.Phonebook.contacts;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Contact {
     private String name;
     private String phone;
 
-    public Contact(String name, String phone) {
-        this.name = name;
-        this.phone = phone;
-    }
-
-    public String getName(){
-        return name;
-    }
-    public String getPhone() { return phone;}
+//    public Contact(String name, String phone) {
+//        this.name = name;
+//        this.phone = phone;
+//    }
+//
+//    public String getName(){
+//        return name;
+//    }
+//    public String getPhone() { return phone;}
 }
